26eea80663090a6ce468a3537a94550fc816a9f4
Tyler Fenton <tsfento@gmail.com>
2024-01-22 13:05:52 -0600

    init commit

---
+++ exercise_one.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Products (
+    ProductID int,
+    ProductName varchar(255),
+    Price float,
+    Category text
+);
+
+INSERT INTO Products (ProductID, ProductName, Price, Category)
+VALUES (1, 'Laptop', 1200.00, 'Electronics'),
+       (2, 'Desk Chair', 250.50, 'Furniture');
+
+SELECT * FROM Products;
